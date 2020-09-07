ae2220aed440e589ea5d976556338d76b04332d8
Morten Vistisen <mv@legalmonster.com>
2020-09-07 14:20:47 +0200

    naming of package was changed for some reason

--- pkg/server.go
+++ pkg/server.go
@@ -1,4 +1,4 @@
-package pkg
+package app
 
 import (
 	"github.com/gin-gonic/gin"
@@ -32,35 +32,4 @@ func (s *Server) Run(addr string) error {
 	}
 
 	return nil
-}
-
-func (s *Server) ApiStatus() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Content-Type", "application/json")
-
-		response := map[string]string{
-			"status": "success",
-			"data":   "vistisen backend api running smootly",
-		}
-
-		c.JSON(http.StatusOK, response)
-	}
-}
-
-func (s *Server) TestMail() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		mail := gomail.NewMessage()
-		mail.SetAddressHeader("From", "hello@mbvistisen.dk", "Vis-ti-sen")
-		mail.SetHeader("To", "vistisen@live.dk")
-		mail.SetHeader("Subject", "Hej fra server")
-
-		err := s.Mailer.DialAndSend(mail)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"status":err})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "great success"})
-	}
 }
